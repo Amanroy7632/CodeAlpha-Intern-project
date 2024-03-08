6019dffe229c02e2d0bbab1c92dcf7596ab486e7
Aman Roy <yadavaman7632@gmail.com>
2024-03-08 23:21:25 +0530

    modified ui

--- ExpenseTrackerApp/index.js
+++ ExpenseTrackerApp/index.js
@@ -1,51 +1,3 @@
-// let totalAmount=8000.0;
-// const total=document.querySelector("#total-amt");
-// total.innerHTML=totalAmount;
-// const form =document.querySelectorAll('input');
-// let expenses=[]
-// const addExpense=(data)=>{
-//   expenses=[data,...expenses];
-//   localStorage.setItem("expenseData",JSON.stringify(data))
-  // const div=document.createElement("div");
-  // div.innerHTML=`
-  //                <div class="item history-common">
-  //                   <span>${data.name}</span><span>${data.date}</span><span class="amt">${data.amount}</span>
-  //                </div>`
-  // document.querySelector(".history-container").appendChild(div);
-//   const amt=document.querySelector(".amt");
-//   console.log(amt);
-//   // if(parseFloat(data.amount)<0)
-//   parseFloat(data.amount)<0 ?amt.style.color='red':amt.style.color='blue'
-// }
-// const subBtn=document.getElementById("subBtn");
-// subBtn.addEventListener("click",(e)=>{
-//   const expDetail={
-//     id:"",
-//     name:"def",
-//     amount:0,
-//     date:"date"
-// }
-//    e.preventDefault();
-//    const date=new Date();
-   
-//    expDetail.id=Math.floor(Math.random()*1000).toString();
-//    expDetail.name=form.item(0).value;
-//    expDetail.amount=parseFloat(form.item(1).value)>0?"+"+parseFloat(form.item(1).value):form.item(1).value;
-//    expDetail.date=date.toLocaleDateString();
-//    addExpense(expDetail)
-//    console.log(expenses);
-//    addAmount(parseFloat(form.item(1).value));
-//    document.querySelector('form').reset();
-//   //  reset();
-// })
-// function reset(){
-//   form.item(0).value='';
-//   form.item(1).value=0;
-// }
-// function addAmount(amount){
-//  totalAmount+=amount
-//  total.innerHTML=totalAmount;
-// }
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -79,9 +31,10 @@ document.addEventListener('DOMContentLoaded', function() {
       const name = document.getElementById('expense-name').value;
       const amount = parseFloat(document.getElementById('expense-amount').value);
       const date = document.getElementById('expense-date').value;
+      const expenseId=Math.floor(Math.random()*10000);
 
       if (name && amount && date) {
-          addExpenseToList(name, amount, date);
+          addExpenseToList(name, amount, date,expenseId);
           if (amount>0) {
             
             totalInc += amount;
@@ -90,13 +43,13 @@ document.addEventListener('DOMContentLoaded', function() {
           }
           totalAmount.innerText=parseFloat(totalAmount.value)+amount;
           updateTotalExpense();
-          saveExpense(name, amount, date);
+          saveExpense(name, amount, date,expenseId);
       }
 
       form.reset();
   });
 
-  function addExpenseToList(name, amount, date) {
+  function addExpenseToList(name, amount, date,expenseId) {
       // const expenseItem = document.createElement('div');
       // expenseItem.classList.add('expense');
       // expenseItem.innerHTML = `
@@ -105,24 +58,27 @@ document.addEventListener('DOMContentLoaded', function() {
       const div=document.createElement("div");
       div.innerHTML=`
                      <div class="item history-common">
-                        <span>${name}</span><span>${date}</span><span class="amt">${amount}</span>
+                        
+                        <input type="text" readonly value="${name}"><input type="date" readonly value="${date}"><input type="number" readonly value="${amount}">
+                        <input type="text" readonly value="${expenseId}" style="display:none;">
+                        <button class="" id="subBtn">✏️</button>
                      </div>`
       document.querySelector(".history-container").appendChild(div);
       // expensesList.appendChild(expenseItem);
   }
 
   function updateTotalExpense() {
-      totalExpense.innerText = `$ ${totalExp.toFixed(2)}`;
-      totalIncome.innerText=`$ ${totalInc.toFixed(2)}`
+      totalExpense.innerText = `🪙 ${totalExp.toFixed(2)}`;
+      totalIncome.innerText=`🪙 ${totalInc.toFixed(2)}`
       totalAmount.innerText=`${(totalInc-totalExp).toFixed(2)}`
   }
 
-  function saveExpense(name, amount, date) {
+  function saveExpense(name, amount, date,expenseId) {
       const expense = {
           name: name,
           amount: amount,
           date: date,
-          id:Math.floor(Math.random()*10000)
+          id:expenseId
       };
       console.log(expenses);
       expenses.push(expense);
