156cd62eeb594d618cc25afe0596f06d71029118
Aman Roy <yadavaman7632@gmail.com>
2024-03-09 00:54:02 +0530

    modified ui with delete option

--- ExpenseTrackerApp/index.js
+++ ExpenseTrackerApp/index.js
@@ -9,13 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
   let totalExp = 0;
   let totalInc=0;
   let expenses = [];
+  let allList;
 
   // Check if there are any expenses stored in local storage
   if (localStorage.getItem('expenses')) {
       expenses = JSON.parse(localStorage.getItem('expenses'));
       // Loop through the stored expenses and display them
       expenses.forEach(expense => {
-          addExpenseToList(expense.name, expense.amount, expense.date);
+          addExpenseToList(expense.name, expense.amount, expense.date,expense.id);
           if(expense.amount>0){
             totalInc+=expense.amount;
           }else{
@@ -45,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
           updateTotalExpense();
           saveExpense(name, amount, date,expenseId);
       }
-
+      allList=document.querySelectorAll("#delbtn");
       form.reset();
   });
 
@@ -55,19 +56,24 @@ document.addEventListener('DOMContentLoaded', function() {
       // expenseItem.innerHTML = `
       //     <strong>${name}</strong> - $${amount.toFixed(2)} - ${date}
       // `;
+      // console.log(expenseId);
       const div=document.createElement("div");
       div.innerHTML=`
                      <div class="item history-common">
                         
-                        <input type="text" readonly value="${name}"><input type="date" readonly value="${date}"><input type="number" readonly value="${amount}">
-                        <input type="text" readonly value="${expenseId}" style="display:none;">
-                        <button class="" id="subBtn">✏️</button>
+                        <input type="text" readonly value="${name}"><input type="date" readonly value="${date}">${amount>0?`<input type="number" style="color:blue;" readonly value="${amount}">`:`<input type="number" style="color:red;" readonly value="${amount}">`} 
+                        <input type="text" readonly value="${expenseId}" style="display:none; id="expId"">
+                        <div> <button class="" id="subBtn">✏️</button>
+                              <button class="" id="delbtn">🗑️</button> 
+                        </div>
                      </div>`
       document.querySelector(".history-container").appendChild(div);
+     
       // expensesList.appendChild(expenseItem);
   }
 
   function updateTotalExpense() {
+    // window.location.reload();
       totalExpense.innerText = `🪙 ${totalExp.toFixed(2)}`;
       totalIncome.innerText=`🪙 ${totalInc.toFixed(2)}`
       totalAmount.innerText=`${(totalInc-totalExp).toFixed(2)}`
@@ -80,8 +86,28 @@ document.addEventListener('DOMContentLoaded', function() {
           date: date,
           id:expenseId
       };
-      console.log(expenses);
+      // console.log(expenses);
       expenses.push(expense);
       localStorage.setItem('expenses', JSON.stringify(expenses));
   }
+    allList=document.querySelectorAll("#delbtn");
+    allList.forEach((item)=>{
+      item.addEventListener('click',()=>{
+        item.parentNode.parentNode.remove()
+        // console.log(item.parentNode.parentNode);
+        expenses= expenses.filter((element)=>{
+          return element.id!=parseInt(item.parentNode.parentNode.children[3].value)
+        })
+        localStorage.setItem('expenses', JSON.stringify(expenses));
+      })
+     
+    })
+  // function deleteExpense(id){
+  //   const allList=document.querySelectorAll("#delbtn");
+  //   allList.forEach((item)=>{
+  //     item.addEventListener('click',()=>{
+  //       const expenseid=item.parentNode.parentNode.childNodes(0);
+  //     })
+  //   })
+  // }
 });
